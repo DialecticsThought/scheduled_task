d1d11acbe0cf22ab093095f8978bdae997734f6f
veritas <1820819809@qq.com>
2024-06-23 16:36:35 +0800

    update

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -3,10 +3,10 @@ create table scheduled_task
     id              bigint auto_increment comment '主键'
         primary key,
     task_id         varbinary(255) null comment '用户输入的唯一标识符',
-    task_name       varchar(255)   null comment '任务名称',
-    cron_expression varchar(255)   null,
-    task_status     varchar(100)   null,
-    deleted         tinyint        null,
+    task_name       varchar(255) null comment '任务名称',
+    cron_expression varchar(255) null,
+    task_status     varchar(100) null,
+    deleted         tinyint null,
     constraint scheduled_task_pk_2
         unique (id)
 );
@@ -15,10 +15,11 @@ create table task_class_dependencies
 (
     id                 bigint auto_increment
         primary key,
-    bean_type          text    null,
-    bean_name          text    null,
-    deleted            tinyint null,
-    task_class_info_id bigint  null
+    bean_type          text null comment '依赖注入的属性的类型',
+    bean_name          text null comment '依赖注入的属性的在容器的名字',
+    task_class_info_id bigint null comment 'task_class_info外键',
+    deleted            tinyint null
+
 );
 
 create table task_class_info
@@ -27,7 +28,7 @@ create table task_class_info
         primary key,
     task_id         varchar(255) null comment '任务id，外键关联',
     task_class_path varchar(255) null comment '类路径',
-    deleted         tinyint      null,
+    deleted         tinyint null,
     constraint task_properties_pk_2
         unique (id)
 );
@@ -36,9 +37,10 @@ create table task_class_properties
 (
     id                 int auto_increment comment '主键'
         primary key,
-    property_name      varchar(255) null,
-    property_value     text         null,
-    deleted            tinyint      null,
-    task_class_info_id bigint       null
+    property_name      varchar(255) null comment '属性名',
+    property_value     text null comment '属性值',
+    task_class_info_id bigint null comment 'task_class_info外键',
+    deleted            tinyint null
+
 );
 
